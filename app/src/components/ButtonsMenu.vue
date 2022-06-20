445da1f17a26fcece6e849433c6d4eae7be4c714
sbarragan <santibarry1930@gmail.com>
2022-06-20 11:46:45 -0500

    added buttonsmenu

---
+++ app/src/components/ButtonsMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-primary">Primary</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-secondary">Secondary</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-success">Success</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-danger">Danger</button>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <button type="button" class="btn btn-link">Link</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonsMenu",
+};
+</script>
+
+<style scoped></style>
--- app/src/views/IndexView.vue
+++ app/src/views/IndexView.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <image-header />
+    <buttons-menu />
   </div>
 </template>
 
 <script>
 import ImageHeader from "@/components/ImageHeader";
+import ButtonsMenu from "@/components/ButtonsMenu";
 export default {
   name: "IndexView",
-  components: { ImageHeader },
+  components: { ButtonsMenu, ImageHeader },
 };
 </script>
