c4a69d17f194dd9210403fa043992152a4a1f131
sbarragan <santibarry1930@gmail.com>
2022-06-20 11:39:32 -0500

    modified image header

--- app/src/components/ImageHeader.vue
+++ app/src/components/ImageHeader.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>hola</div>
+  <div>
+    <img
+      src="https://d15mvavv27jnvy.cloudfront.net//70de6c5b5efac9e61fd856691fb5854e.jpg"
+      alt="image"
+      class="img-thumbnail rounded-circle img-fluid"
+      style="height: 150px"
+    />
+    <p class="mt-3 fw-bold">@bazar.de.belleza</p>
+  </div>
 </template>
 
 <script>
