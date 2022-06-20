569627357ebb5aa3757f9c872961cf62adddaf4c
sbarragan <santibarry1930@gmail.com>
2022-06-20 11:28:22 -0500

    added index component

---
+++ app/src/components/ImageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>hola</div>
+</template>
+
+<script>
+export default {
+  name: "ImageHeader",
+};
+</script>
---
+++ app/src/views/IndexView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <image-header />
+  </div>
+</template>
+
+<script>
+import ImageHeader from "@/components/ImageHeader";
+export default {
+  name: "IndexView",
+  components: { ImageHeader },
+};
+</script>
